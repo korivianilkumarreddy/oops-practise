0760bc7efd2ae3e6b181f97217061c9bb95014db
Anil Kumar Reddy <korivianilkumar3000@gmail.com>
2025-06-14 18:39:05 +0530

    completed untill chatbook created

--- oops_proj.py
+++ oops_proj.py
@@ -11,15 +11,17 @@ class chatbook:
                            2. press 2 to signin
                            3. press 3 to write a post 
                            4. press 4 to message a frnd
-                           5. press any other key to exit\n""")
+                           5. press any other key to exit
+                           
+                           -->""")
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.sendmsg()
         else :
             exit()    
     # signup function created......
@@ -46,9 +48,28 @@ class chatbook:
             else:
                 print("plz input correct credentials..")
         print("\n")
-        self.menu()            
+        self.menu() 
 
-        
+    # my_post function created
+    def my_post(self):
+        if self.loggedin == True:
+            txt = input("enter your message here ->")
+            print(f"following content has been posted-> {txt}")
+        else:
+            print("you need to signin first to post something...")
+        print("\n")
+        self.menu()
+
+    # sendmsg function has created
+    def sendmsg(self):
+        if self.loggedin == True:
+            txt = input("enter your message here->")
+            frnd = input("whom to send the msg..?-->")
+            print(f"your msg has been sent to {frnd}")
+        else:
+            print("you need to signin first to post something..")
+        print("\n")
+        self.menu()        
 
 
-obj = chatbook()            
+user1 = chatbook()            
--- rough.py
+++ rough.py
@@ -1,6 +1,8 @@
-my_int = 1
+'''my_int = 1
 lst = [1,2,3]
 my_str = "data_science_academy"
 lst.clear()
 print(lst)
-#print(type(lst))
+#print(type(lst))'''
+from oops_proj import chatbook
+user1 = chatbook()
